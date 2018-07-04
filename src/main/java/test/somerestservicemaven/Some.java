85cb05df739ff6774ec6fc4b024f5594d8e2f4cf
Yaroslav <my@my.ru>
2018-07-04 23:13:16 +0300

    init

---
+++ src/main/java/test/somerestservicemaven/Some.java
@@ -0,0 +1,13 @@
+package test.somerestservicemaven;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Some {
+
+    @GetMapping
+    public String test(){
+        return "OK";
+    }
+}
---
+++ src/main/java/test/somerestservicemaven/SomeRestServiceMavenApplication.java
@@ -0,0 +1,12 @@
+package test.somerestservicemaven;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SomeRestServiceMavenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SomeRestServiceMavenApplication.class, args);
+	}
+}
